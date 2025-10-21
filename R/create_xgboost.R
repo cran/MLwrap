1c72e346bc30f0f5e81463a43bd1956ef78811a7
Albert Sesé <albert.sese@uib.es>
2025-10-21 15:10:02 +0000

    version 0.2.1

--- R/create_xgboost.R
+++ R/create_xgboost.R
@@ -4,8 +4,8 @@ create_xgboost <- function(hyperparams, task){
   trees <- if (hyperparams$trees_tune) tune::tune() else as.integer(hyperparams$hyperparams_constant$trees)
   min_n <- if (hyperparams$min_n_tune) tune::tune() else as.integer(hyperparams$hyperparams_constant$min_n)
   tree_depth <- if (hyperparams$tree_depth_tune) tune::tune() else as.integer(hyperparams$hyperparams_constant$tree_depth)
-  learn_rate <- if (hyperparams$learn_rate_tune) tune::tune() else as.integer(hyperparams$hyperparams_constant$learn_rate)
-  loss_reduction <- if (hyperparams$loss_reduction_tune) tune::tune() else as.integer(hyperparams$hyperparams_constant$loss_reduction)
+  learn_rate <- if (hyperparams$learn_rate_tune) tune::tune() else as.numeric(hyperparams$hyperparams_constant$learn_rate)
+  loss_reduction <- if (hyperparams$loss_reduction_tune) tune::tune() else as.numeric(hyperparams$hyperparams_constant$loss_reduction)
 
   model = parsnip::boost_tree(
 
